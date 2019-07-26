2102f3a677d8843b9f271718c3aae999aa47379a
danilo <dev.daniloweb@gmail.com>
2019-07-26 14:40:45 -0300

    Database settings

---
+++ src/config/database.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+
+module.exports = {
+  dialect: process.env.DIALECT,
+  host: process.env.HOST,
+  username: process.env.USERNAME,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  operetorAliases: false,
+  define: {
+    timestemps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+}
