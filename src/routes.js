74e1e4d41da034c86f5b2334f77fecd0f54bc367
danilo <dev.daniloweb@gmail.com>
2019-07-27 15:23:42 -0300

    User registration created

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,11 @@
 const routes = require('express').Router()
+const multerConfig = require('./config/multer')
+const multer = require('multer')
+const upload = multer({ storage: multerConfig })
+
+const UserController = require('./app/controllers/Usercontroller')
+
+routes.get('/signup', UserController.create)
+routes.post('/signup', upload.single('avatar'), UserController.store)
 
 module.exports = routes
