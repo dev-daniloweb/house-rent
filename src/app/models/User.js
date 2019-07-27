f593df54dca60876752dce0821e1401dd766c09a
danilo <dev.daniloweb@gmail.com>
2019-07-27 15:24:30 -0300

    Add password encryption

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,9 +1,20 @@
+const bcrypt = require('bcryptjs')
+
 module.exports = (sequelize, DataType) => {
   const User = sequelize.define('User', {
     name: DataType.STRING,
     email: DataType.STRING,
+    avatar: DataType.STRING,
     password: DataType.VIRTUAL,
     password_hash: DataType.STRING
+  }, {
+    hooks: {
+      beforeSave: async user => {
+        if (user.password) {
+          user.password_hash = await bcrypt.hash(user.password, 8)
+        }
+      }
+    }
   })
 
   return User
